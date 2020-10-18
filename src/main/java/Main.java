1ca996b189e5d7fb2a445382a0903f6eba1cbe33
Patrick Dusk <patrick.bohoni1114@gmail.com>
2020-10-18 13:08:46 +0300

    Add sout to test new branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello Radu");
+        System.out.println("Change for test branch");
     }
 }
