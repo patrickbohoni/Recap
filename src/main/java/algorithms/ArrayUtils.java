76556efd8d41c579746951d4ef3d3f975905286b
Patrick Dusk <patrick.bohoni1114@gmail.com>
2020-10-29 21:08:01 +0200

    done until exercise 4

--- src/main/java/algorithms/ArrayUtils.java
+++ src/main/java/algorithms/ArrayUtils.java
@@ -3,21 +3,102 @@ package algorithms;
 public class ArrayUtils {
 
   public static boolean isPalindrome(int[] n) {
-      boolean isPalindrome = false;
-      //palindrom: int[] n = { 1, 2, 5, 5, 2, 1};
-     // i :n[0], n[1], n[2], n[3], n[4], n[5]
-      //j: n[5], n[4], n[3], n[2], n[1], n[0]
-      // i: 0, 1 ,2
-      //j: 2, 1, 0
-      for (int i = 0 , j = n.length - 1; i <= j ; i++, j--) {
-          if(n[i] != n[j]) {
-              isPalindrome = false;
-              break;
-          } else {
-              isPalindrome = true;
+//      boolean isPalindrome = false;
+//      //palindrom: int[] n = { 1, 2, 5, 5, 2, 1};
+//     // i :n[0], n[1], n[2], n[3], n[4], n[5]
+//      //j: n[5], n[4], n[3], n[2], n[1], n[0]
+//      // i: 0, 1 ,2
+//      //j: 2, 1, 0
+//      for (int i = 0 , j = n.length - 1; i <= j ; i++, j--) {
+//          if(n[i] != n[j]) {
+//              isPalindrome = false;
+//              break;
+//          } else {
+//              isPalindrome = true;
+//          }
+//      }
+//      return isPalindrome;
+//  }     //asta e exercitiul facut la curs - it works
+
+      for (int i = 0, j = n.length -1 ; i <= j ; i++, j--) {
+          if(n[i] != n[j]){
+              return false;
           }
       }
-      return isPalindrome;
-  }
+          return true;
+      }
+
+      //exercitiul 1
+    public static void reverseArray(int[] a, int arrLength) {
+      int[] revArray = new int[arrLength];
+      int j = arrLength;
+        for (int i = 0; i < arrLength ; i++) {
+            revArray[j -1] = a[i];
+            j = j - 1;
+        }
+
+        System.out.println("Elementele inversate sunt: \n");
+        for (int k = 0; k < arrLength; k++) {
+            System.out.println(revArray[k]);
+        }
+    }
+
+    //exercitiul 2 - array-ul se sorteaza aducand cel mai mic numar la index cu fiecare repetare a celor
+    // doua for loop-uri, nu e cel mai eficient mod de a sorta, dar a fost cel dat in exemplu
+    public static void selectionSort(int[] selectedArray) {
+        for (int i = 0; i < selectedArray.length ; i++) {
+            int index = i;
+            for (int j = i + 1; j < selectedArray.length ; j++) {
+                if(selectedArray[j] < selectedArray[index]) {
+                    index = j;
+                }
+            }
+            int smallerNumber = selectedArray[index];
+            selectedArray[index] = selectedArray[i];
+            selectedArray[i] = smallerNumber;
+        }
+    }
+
+    //exercitiul 3
+    public static boolean isPrime(int n) {
+      //verificam cazurile extreme ca n sa nu fie 1 sau 2
+        if (n <= 1) {
+            return false;
+        }
+        if (n <= 3) {
+            return true;
+        }
+        if (n % 2 == 0 || n % 3 == 0) {
+            return false;
+        }
+
+        //aici verificam ca numarul sa fie de forma 6k+-1, toate primele cu exceptia lui 2 sau 3 au
+        //acest lucru in comun.
+        for (int i = 5; i * i <= n; i = i + 6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //exercitiul 4
+    public static int removeDuplicateElements(int array[], int n) {
+      int j = 0;
+        for (int i = 0; i < n -1; i++) {
+            if (array[i] != array[i+1]) {
+                array[j++] = array[i];
+            }
+        }
+        array[j++] = array[n -1];
+        return j;
+    }
+
+
+
+
+
+
+
 
 }
--- src/main/java/algorithms/Main.java
+++ src/main/java/algorithms/Main.java
@@ -1,14 +1,54 @@
 package algorithms;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
         int[] e1 = { 1, 2, 5, 5, 2, 1};
         int[] e2 = { 2, 1, 1, 3, 2, 1};
+        int[] e3 = { 1, 3, 5, 3, 1};
+        int[] e4 = { 4, 8, 7};
+        int[] e5 = { 10, 20, 20, 30, 40, 50, 50};
 
         System.out.println(ArrayUtils.isPalindrome(e1));
         System.out.println(ArrayUtils.isPalindrome(e2));
+        System.out.println(ArrayUtils.isPalindrome(e3));
+        System.out.println(ArrayUtils.isPalindrome(e4));
+
+
+        //aplicat exercitiul 1
+        ArrayUtils.reverseArray(e4, e4.length);
+        System.out.println(" " + " ");
+
+
+        //aplicat exercitiul 2
+        ArrayUtils.selectionSort(e1);
+        System.out.println("The sorted elements are: ");
+        for (int i:e1) {
+            System.out.println(i + "");
+        }
+
+        //aplicat exercitiul 3
+        System.out.println(" \n");
+        System.out.println(ArrayUtils.isPrime(73));
+        System.out.println(ArrayUtils.isPrime(21));
+
+        //aplicat exercitiul 4 -- nota: array-ul trebuie sortat, altfel nu se exclud duplicatele
+        System.out.println(" \n");
+        Arrays.sort(e2);
+        int length = e2.length;
+        length = ArrayUtils.removeDuplicateElements(e2, length);
+        for (int i = 0; i < length ; i++) {
+            System.out.println(e2[i] + " ");
+        }
+
+
+
+
+
+
 
 
     }
